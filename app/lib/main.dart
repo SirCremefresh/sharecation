d97360161e96d7a81dfdbd139a251ecd1f0f7651
donatowolfisberg <donato@wolfisberg.dev>
2022-05-05 23:40:12 +0200

    fix flutter analyzer bug

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
                 Navigator.of(context).pushReplacementNamed('/sign-in');
               }),
             ])),
-        '/camera': (context) => CameraScreen(),
+        '/camera': (context) => const CameraScreen(),
       },
     );
   }

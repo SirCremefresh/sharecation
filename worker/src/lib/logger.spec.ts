9343058f7e69db7d617eecf6052e57dc407da6e5
donatowolfisberg <donato@wolfisberg.dev>
2022-05-08 17:22:56 +0200

    more tests

--- worker/src/lib/logger.spec.ts
+++ worker/src/lib/logger.spec.ts
@@ -14,16 +14,19 @@ function mockLoggerContext() {
 
 test('logError without error object should call loggerContext', async () => {
   const {errorCalls, loggerContext} = mockLoggerContext();
+  jest.spyOn(console, 'error').mockImplementation();
 
   logError('some-message', loggerContext);
 
   expect(errorCalls).toHaveLength(1);
   expect(errorCalls[0][0]).toEqual('some-message');
   expect(errorCalls[0][1]).toEqual(undefined);
+  expect(console.error).toBeCalledTimes(0);
 });
 
 test('logError with error object should call loggerContext', async () => {
   const {errorCalls, loggerContext} = mockLoggerContext();
+  jest.spyOn(console, 'error').mockImplementation();
   const error = new Error('some-error');
 
   logError('some-message', error, loggerContext);
@@ -31,4 +34,14 @@ test('logError with error object should call loggerContext', async () => {
   expect(errorCalls).toHaveLength(1);
   expect(errorCalls[0][0]).toEqual('some-message');
   expect(errorCalls[0][1]).toEqual(error);
+  expect(console.error).toBeCalledTimes(0);
+});
+
+test('logError without loggerContext should log to console', async () => {
+  jest.spyOn(console, 'error').mockImplementation();
+
+  logError('some-message', {});
+
+  expect(console.error).toBeCalledTimes(1);
+  expect(console.error).toBeCalledWith('some-message error=undefined', undefined);
 });

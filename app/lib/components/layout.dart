d685befae8ed0d44ccd0225744991af1561e7500
donatowolfisberg <donato@wolfisberg.dev>
2022-05-16 21:51:58 +0200

    display groups in drawer

--- app/lib/components/layout.dart
+++ app/lib/components/layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:sharecation_app/api/contracts/groups/v1/groups.pbserver.dart';
 import 'package:sharecation_app/service/api_service.dart';
 
 class Layout extends StatelessWidget {
@@ -14,6 +16,7 @@ class Layout extends StatelessWidget {
         title: const Text('Sharecation'),
       ),
       body: child,
+      drawer: buildDrawer(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
           if (index == 0) {
@@ -44,11 +47,58 @@ class Layout extends StatelessWidget {
         onPressed: () async {
           final ImagePicker _picker = ImagePicker();
           final XFile? photo =
-              await _picker.pickImage(source: ImageSource.camera);
+          await _picker.pickImage(source: ImageSource.camera);
           if (photo != null) {
             await api.images.uploadImage(photo);
           }
         },
+      ),
+    );
+  }
+
+  Drawer buildDrawer() {
+    return Drawer(
+      child: FutureBuilder<List<Group>>(
+        future: api.groups.getGroups(),
+        initialData: const [],
+        builder: (context, data) {
+          if (!data.hasData) {
+            return ListView(
+              children: [
+                buildDrawerHeader(),
+                const LoadingIndicator(
+                  borderWidth: 4,
+                  size: 50,
+                )
+              ],
+            );
+          }
+          var groups = data.data!;
+          return ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: groups.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return buildDrawerHeader();
+                }
+                return TextButton(
+                  onPressed: () {},
+                  child: Text(groups[index - 1].name),
+                );
+              });
+        },
+      ),
+    );
+  }
+
+  SizedBox buildDrawerHeader() {
+    return const SizedBox(
+      height: 64.0,
+      child: DrawerHeader(
+        child: Text('Groups', style: TextStyle(color: Colors.white)),
+        decoration: BoxDecoration(color: Colors.black),
+        margin: EdgeInsets.all(0.0),
+        padding: EdgeInsets.all(0.0),
       ),
     );
   }

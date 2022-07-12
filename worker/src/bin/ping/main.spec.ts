789c7f913c6fb3b7df2469c340ed34d4e6d18e5d
donatowolfisberg <donato@wolfisberg.dev>
2022-07-12 22:59:58 +0200

    build code from test

--- worker/src/bin/ping/main.spec.ts
+++ worker/src/bin/ping/main.spec.ts
@@ -1,33 +1,38 @@
-import {Miniflare} from 'miniflare';
 import {describe, test} from '@jest/globals';
+import {build} from 'esbuild';
+import {Miniflare} from 'miniflare';
 
+const out = await build({
+  entryPoints: ['src/bin/ping/main.ts'],
+  outfile: 'src/bin/ping/dist/main2.js',
+  bundle: true,
+  format: 'esm',
+});
+console.log(out)
 const mf = new Miniflare({
-  envPath: true,
-  packagePath: true,
-  wranglerConfigPath: true,
-  scriptPath: './src/bin/ping/dist/main.js',
-  modules: true,
+  envPath: true, packagePath: true, wranglerConfigPath: true, // scriptPath: './src/bin/ping/dist/main.js',
+  scriptPath: 'src/bin/ping/dist/main2.js', modules: true,
 });
 //
 describe('Ping', () => {
   test('Should return pong with given pingId1', async () => {
-  await mf.reload();
+    await mf.reload();
     const res = await mf.dispatchFetch('http://localhost:8787/').then(res => res.text());
-    console.log(res)
+    console.log(res);
   });
   test('Should return pong with given pingId2', async () => {
-  await mf.reload();
+    await mf.reload();
     const res = await mf.dispatchFetch('http://localhost:8787/').then(res => res.text());
-    console.log(res)
+    console.log(res);
   });
   test('Should return pong with given pingId3', async () => {
-  await mf.reload();
+    await mf.reload();
     const res = await mf.dispatchFetch('http://localhost:8787/').then(res => res.text());
-    console.log(res)
+    console.log(res);
   });
   test('Should return pong with given pingId4', async () => {
-  await mf.reload();
+    await mf.reload();
     const res = await mf.dispatchFetch('http://localhost:8787/').then(res => res.text());
-    console.log(res)
+    console.log(res);
   });
 });

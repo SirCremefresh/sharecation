e0faee8333d1cb6bf7fa66f9efe3b8f141d04217
donatowolfisberg <donato@wolfisberg.dev>
2022-07-13 21:38:34 +0200

    fix typescript

--- worker/src/lib/starter/on-durable-object-fetch.ts
+++ worker/src/lib/starter/on-durable-object-fetch.ts
@@ -1,22 +1,34 @@
-import { BasicError_BasicErrorCode } from '../../contracts/errors/v1/errors';
-import { createBasicErrorResponse } from '../http/response';
-import { logError } from '../logger';
-import { RequestIdContext } from '../middleware/context';
-import { addRequestId } from '../middleware/request-id-middleware';
+import {BasicError_BasicErrorCode} from '../../contracts/errors/v1/errors';
+import {createBasicErrorResponse} from '../http/response';
+import {logError} from '../logger';
+import {BaseContext, RequestIdContext} from '../middleware/context';
+import {addRequestId} from '../middleware/request-id-middleware';
+
+declare global {
+  // noinspection ES6ConvertVarToLetConst
+  var TESTING_BASE_CONTEXT: BaseContext | undefined | null;
+}
+
+const BASE_CONTEXT: BaseContext = {
+  base: {
+    fetch: (request: Request | string,
+            requestInitr?: RequestInit | Request) => fetch(request, requestInitr),
+  }
+};
 
 export function onDurableObjectFetch<ENV extends {} = {}>(
   envGetter: () => ENV,
   fn: (
     request: Request,
     env: ENV,
-    context: RequestIdContext,
+    context: RequestIdContext & BaseContext,
   ) => Promise<Response>,
 ) {
   const packedFn = addRequestId(fn);
   return async (request: Request) => {
     // env needs to be fetched every request because it is still null on initializing function
     const env = envGetter();
-    const context = {};
+    let context = Object.assign({}, BASE_CONTEXT);
     try {
       return await packedFn(request, env, context);
     } catch (e) {

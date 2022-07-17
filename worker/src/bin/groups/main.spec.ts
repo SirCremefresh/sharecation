a1d401e16a00ac0d1c670f8df5f8c50908fda128
donatowolfisberg <donato@wolfisberg.dev>
2022-07-17 23:05:45 +0200

    added stub tests

--- worker/src/bin/groups/main.spec.ts
+++ worker/src/bin/groups/main.spec.ts
@@ -154,4 +154,289 @@ describe('Groups', () => {
     const pingResponse = unwrapOk(responseBody);
     expect(pingResponse.name).toEqual('Some-cool-name');
   });
+  test('create group2', async () => {
+    const responsqwere: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+      userId:'some-user-id',
+      role:'some-role-id',
+    });
+    console.log(CreateRoleBindingResponse.toJsonString(responsqwere))
+
+
+    const fetchStub = new FetchStub();
+    fetchStub.addStub(new URLPattern('https://logs-prod-eu-west-0.grafana.net/loki/api/v1/push'),
+      async (request, requestInitr) => {
+        console.log(requestInitr);
+        return new Response('{}', {status: 200});
+      });
+    fetchStub.addStub(new URLPattern('https://sharecation-authentication-development.dowo.ch/v1/create-role-binding'),
+      async (request, requestInitr) => {
+        const response: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+          userId:'some-user-id',
+          role:'some-role-id',
+        });
+        return new Response(CreateRoleBindingResponse.toJsonString(response), {status: 200});
+      });
+    const {privateKey, publicKey} = await generateKeys();
+    const {publicJkw} = await exportPublicAndPrivateInJwk(publicKey, privateKey);
+    const {jwtString} = await generateJwt('some', [], privateKey, publicJkw.kid, Math.floor(Date.now() / 1000) + 60 * 60);
+
+    await mf.setOptions({
+      globals: {
+        TESTING_BASE_CONTEXT: {
+          base: {
+            fetch: fetchStub.getFetch()
+          }
+        }
+      },
+      bindings: {
+        ENVIRONMENT: 'testing',
+        LOKI_SECRET: 'some-secret',
+        PUBLIC_KEYS: JSON.stringify([publicJkw])
+      },
+    });
+
+    const requst: CreateGroupRequest = {
+      name: 'Some-cool-name'
+    };
+
+    const response = await mf.dispatchFetch('https://fake.url/v1/create-group', {
+      method: 'POST',
+      body: CreateGroupRequest.toJsonString(requst),
+      headers: {
+        'Authorization': `Bearer ${jwtString}`,
+      }
+    });
+
+    const responseBody = CreateGroupResponse.fromJsonString(await response.text());
+    const pingResponse = unwrapOk(responseBody);
+    expect(pingResponse.name).toEqual('Some-cool-name');
+  });
+  test('create group3', async () => {
+    const responsqwere: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+      userId:'some-user-id',
+      role:'some-role-id',
+    });
+    console.log(CreateRoleBindingResponse.toJsonString(responsqwere))
+
+
+    const fetchStub = new FetchStub();
+    fetchStub.addStub(new URLPattern('https://logs-prod-eu-west-0.grafana.net/loki/api/v1/push'),
+      async (request, requestInitr) => {
+        console.log(requestInitr);
+        return new Response('{}', {status: 200});
+      });
+    fetchStub.addStub(new URLPattern('https://sharecation-authentication-development.dowo.ch/v1/create-role-binding'),
+      async (request, requestInitr) => {
+        const response: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+          userId:'some-user-id',
+          role:'some-role-id',
+        });
+        return new Response(CreateRoleBindingResponse.toJsonString(response), {status: 200});
+      });
+    const {privateKey, publicKey} = await generateKeys();
+    const {publicJkw} = await exportPublicAndPrivateInJwk(publicKey, privateKey);
+    const {jwtString} = await generateJwt('some', [], privateKey, publicJkw.kid, Math.floor(Date.now() / 1000) + 60 * 60);
+
+    await mf.setOptions({
+      globals: {
+        TESTING_BASE_CONTEXT: {
+          base: {
+            fetch: fetchStub.getFetch()
+          }
+        }
+      },
+      bindings: {
+        ENVIRONMENT: 'testing',
+        LOKI_SECRET: 'some-secret',
+        PUBLIC_KEYS: JSON.stringify([publicJkw])
+      },
+    });
+
+    const requst: CreateGroupRequest = {
+      name: 'Some-cool-name'
+    };
+
+    const response = await mf.dispatchFetch('https://fake.url/v1/create-group', {
+      method: 'POST',
+      body: CreateGroupRequest.toJsonString(requst),
+      headers: {
+        'Authorization': `Bearer ${jwtString}`,
+      }
+    });
+
+    const responseBody = CreateGroupResponse.fromJsonString(await response.text());
+    const pingResponse = unwrapOk(responseBody);
+    expect(pingResponse.name).toEqual('Some-cool-name');
+  });
+  test('create group4', async () => {
+    const responsqwere: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+      userId:'some-user-id',
+      role:'some-role-id',
+    });
+    console.log(CreateRoleBindingResponse.toJsonString(responsqwere))
+
+
+    const fetchStub = new FetchStub();
+    fetchStub.addStub(new URLPattern('https://logs-prod-eu-west-0.grafana.net/loki/api/v1/push'),
+      async (request, requestInitr) => {
+        console.log(requestInitr);
+        return new Response('{}', {status: 200});
+      });
+    fetchStub.addStub(new URLPattern('https://sharecation-authentication-development.dowo.ch/v1/create-role-binding'),
+      async (request, requestInitr) => {
+        const response: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+          userId:'some-user-id',
+          role:'some-role-id',
+        });
+        return new Response(CreateRoleBindingResponse.toJsonString(response), {status: 200});
+      });
+    const {privateKey, publicKey} = await generateKeys();
+    const {publicJkw} = await exportPublicAndPrivateInJwk(publicKey, privateKey);
+    const {jwtString} = await generateJwt('some', [], privateKey, publicJkw.kid, Math.floor(Date.now() / 1000) + 60 * 60);
+
+    await mf.setOptions({
+      globals: {
+        TESTING_BASE_CONTEXT: {
+          base: {
+            fetch: fetchStub.getFetch()
+          }
+        }
+      },
+      bindings: {
+        ENVIRONMENT: 'testing',
+        LOKI_SECRET: 'some-secret',
+        PUBLIC_KEYS: JSON.stringify([publicJkw])
+      },
+    });
+
+    const requst: CreateGroupRequest = {
+      name: 'Some-cool-name'
+    };
+
+    const response = await mf.dispatchFetch('https://fake.url/v1/create-group', {
+      method: 'POST',
+      body: CreateGroupRequest.toJsonString(requst),
+      headers: {
+        'Authorization': `Bearer ${jwtString}`,
+      }
+    });
+
+    const responseBody = CreateGroupResponse.fromJsonString(await response.text());
+    const pingResponse = unwrapOk(responseBody);
+    expect(pingResponse.name).toEqual('Some-cool-name');
+  });
+  test('create group5', async () => {
+    const responsqwere: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+      userId:'some-user-id',
+      role:'some-role-id',
+    });
+    console.log(CreateRoleBindingResponse.toJsonString(responsqwere))
+
+
+    const fetchStub = new FetchStub();
+    fetchStub.addStub(new URLPattern('https://logs-prod-eu-west-0.grafana.net/loki/api/v1/push'),
+      async (request, requestInitr) => {
+        console.log(requestInitr);
+        return new Response('{}', {status: 200});
+      });
+    fetchStub.addStub(new URLPattern('https://sharecation-authentication-development.dowo.ch/v1/create-role-binding'),
+      async (request, requestInitr) => {
+        const response: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+          userId:'some-user-id',
+          role:'some-role-id',
+        });
+        return new Response(CreateRoleBindingResponse.toJsonString(response), {status: 200});
+      });
+    const {privateKey, publicKey} = await generateKeys();
+    const {publicJkw} = await exportPublicAndPrivateInJwk(publicKey, privateKey);
+    const {jwtString} = await generateJwt('some', [], privateKey, publicJkw.kid, Math.floor(Date.now() / 1000) + 60 * 60);
+
+    await mf.setOptions({
+      globals: {
+        TESTING_BASE_CONTEXT: {
+          base: {
+            fetch: fetchStub.getFetch()
+          }
+        }
+      },
+      bindings: {
+        ENVIRONMENT: 'testing',
+        LOKI_SECRET: 'some-secret',
+        PUBLIC_KEYS: JSON.stringify([publicJkw])
+      },
+    });
+
+    const requst: CreateGroupRequest = {
+      name: 'Some-cool-name'
+    };
+
+    const response = await mf.dispatchFetch('https://fake.url/v1/create-group', {
+      method: 'POST',
+      body: CreateGroupRequest.toJsonString(requst),
+      headers: {
+        'Authorization': `Bearer ${jwtString}`,
+      }
+    });
+
+    const responseBody = CreateGroupResponse.fromJsonString(await response.text());
+    const pingResponse = unwrapOk(responseBody);
+    expect(pingResponse.name).toEqual('Some-cool-name');
+  });
+  test('create group6', async () => {
+    const responsqwere: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+      userId:'some-user-id',
+      role:'some-role-id',
+    });
+    console.log(CreateRoleBindingResponse.toJsonString(responsqwere))
+
+
+    const fetchStub = new FetchStub();
+    fetchStub.addStub(new URLPattern('https://logs-prod-eu-west-0.grafana.net/loki/api/v1/push'),
+      async (request, requestInitr) => {
+        console.log(requestInitr);
+        return new Response('{}', {status: 200});
+      });
+    fetchStub.addStub(new URLPattern('https://sharecation-authentication-development.dowo.ch/v1/create-role-binding'),
+      async (request, requestInitr) => {
+        const response: CreateRoleBindingResponse = createProtoBufOkResponse<RoleBinding>({
+          userId:'some-user-id',
+          role:'some-role-id',
+        });
+        return new Response(CreateRoleBindingResponse.toJsonString(response), {status: 200});
+      });
+    const {privateKey, publicKey} = await generateKeys();
+    const {publicJkw} = await exportPublicAndPrivateInJwk(publicKey, privateKey);
+    const {jwtString} = await generateJwt('some', [], privateKey, publicJkw.kid, Math.floor(Date.now() / 1000) + 60 * 60);
+
+    await mf.setOptions({
+      globals: {
+        TESTING_BASE_CONTEXT: {
+          base: {
+            fetch: fetchStub.getFetch()
+          }
+        }
+      },
+      bindings: {
+        ENVIRONMENT: 'testing',
+        LOKI_SECRET: 'some-secret',
+        PUBLIC_KEYS: JSON.stringify([publicJkw])
+      },
+    });
+
+    const requst: CreateGroupRequest = {
+      name: 'Some-cool-name'
+    };
+
+    const response = await mf.dispatchFetch('https://fake.url/v1/create-group', {
+      method: 'POST',
+      body: CreateGroupRequest.toJsonString(requst),
+      headers: {
+        'Authorization': `Bearer ${jwtString}`,
+      }
+    });
+
+    const responseBody = CreateGroupResponse.fromJsonString(await response.text());
+    const pingResponse = unwrapOk(responseBody);
+    expect(pingResponse.name).toEqual('Some-cool-name');
+  });
 });

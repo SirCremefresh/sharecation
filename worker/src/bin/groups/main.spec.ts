278aa3cdd1758a075d6a401b63b6187d472e6da4
donatowolfisberg <donato@wolfisberg.dev>
2022-08-18 21:45:33 +0200

    get existing groups test

--- worker/src/bin/groups/main.spec.ts
+++ worker/src/bin/groups/main.spec.ts
@@ -8,7 +8,7 @@ import {getTestingContainer} from '../../test-lib/test-container';
 const testContainer = await getTestingContainer('groups');
 
 describe('Groups', () => {
-  test('get groups', async () => {
+  test('get groups has no groups', async () => {
     const testRun = await testContainer.initTest();
     const jwtString = await testRun.getJwt({roles: []});
 
@@ -20,9 +20,31 @@ describe('Groups', () => {
       }
     });
 
-    const responseBody = GetGroupsResponse.fromJsonString(await response.text());
-    const pingResponse = unwrapOk(responseBody);
+    const pingResponse = unwrapOk(GetGroupsResponse.fromJsonString(await response.text()));
     expect(pingResponse.groups.length).toEqual(0);
+  });
+
+  test('get groups with two groups', async () => {
+    const testRun = await testContainer.initTest();
+    const jwtString = await testRun.getJwt({roles: ['groups:group-id-1']});
+    let kvNamespace = await testContainer.mf.getKVNamespace('GROUPS');
+    await kvNamespace.put('groups:group-id-1:', JSON.stringify({
+      groupId: 'group-id-1',
+      name: 'group-name-1',
+    }));
+    console.log(jwtString)
+    // getTypedKVInstance<AuthenticationKv>(await testContainer.mf.getKVNamespace('GROUPS') as KVNamespace);
+
+    const response = await testRun.dispatchFetch('https://fake.url/v1/get-groups', {
+      method: 'POST',
+      body: null,
+      headers: {
+        'Authorization': `Bearer ${jwtString}`,
+      }
+    });
+
+    const pingResponse = unwrapOk(GetGroupsResponse.fromJsonString(await response.text()));
+    expect(pingResponse.groups.length).toEqual(1);
   });
 
   test('create group', async () => {

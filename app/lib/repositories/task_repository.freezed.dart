daa6dade6750611d9132929f0d61f77718f622de
donatowolfisberg <donato@wolfisberg.dev>
2022-09-20 22:27:24 +0200

    adding task repository

--- app/lib/repositories/task_repository.freezed.dart
+++ app/lib/repositories/task_repository.freezed.dart
@@ -14,42 +14,220 @@ T _$identity<T>(T value) => value;
 final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
 
+/// @nodoc
+mixin _$TaskRun {
+  TaskConfig get taskConfig => throw _privateConstructorUsedError;
+  String get taskRunId => throw _privateConstructorUsedError;
+  TaskState get taskState => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $TaskRunCopyWith<TaskRun> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TaskRunCopyWith<$Res> {
+  factory $TaskRunCopyWith(TaskRun value, $Res Function(TaskRun) then) =
+      _$TaskRunCopyWithImpl<$Res>;
+  $Res call({TaskConfig taskConfig, String taskRunId, TaskState taskState});
+
+  $TaskConfigCopyWith<$Res> get taskConfig;
+}
+
+/// @nodoc
+class _$TaskRunCopyWithImpl<$Res> implements $TaskRunCopyWith<$Res> {
+  _$TaskRunCopyWithImpl(this._value, this._then);
+
+  final TaskRun _value;
+  // ignore: unused_field
+  final $Res Function(TaskRun) _then;
+
+  @override
+  $Res call({
+    Object? taskConfig = freezed,
+    Object? taskRunId = freezed,
+    Object? taskState = freezed,
+  }) {
+    return _then(_value.copyWith(
+      taskConfig: taskConfig == freezed
+          ? _value.taskConfig
+          : taskConfig // ignore: cast_nullable_to_non_nullable
+              as TaskConfig,
+      taskRunId: taskRunId == freezed
+          ? _value.taskRunId
+          : taskRunId // ignore: cast_nullable_to_non_nullable
+              as String,
+      taskState: taskState == freezed
+          ? _value.taskState
+          : taskState // ignore: cast_nullable_to_non_nullable
+              as TaskState,
+    ));
+  }
+
+  @override
+  $TaskConfigCopyWith<$Res> get taskConfig {
+    return $TaskConfigCopyWith<$Res>(_value.taskConfig, (value) {
+      return _then(_value.copyWith(taskConfig: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_TaskRunCopyWith<$Res> implements $TaskRunCopyWith<$Res> {
+  factory _$$_TaskRunCopyWith(
+          _$_TaskRun value, $Res Function(_$_TaskRun) then) =
+      __$$_TaskRunCopyWithImpl<$Res>;
+  @override
+  $Res call({TaskConfig taskConfig, String taskRunId, TaskState taskState});
+
+  @override
+  $TaskConfigCopyWith<$Res> get taskConfig;
+}
+
+/// @nodoc
+class __$$_TaskRunCopyWithImpl<$Res> extends _$TaskRunCopyWithImpl<$Res>
+    implements _$$_TaskRunCopyWith<$Res> {
+  __$$_TaskRunCopyWithImpl(_$_TaskRun _value, $Res Function(_$_TaskRun) _then)
+      : super(_value, (v) => _then(v as _$_TaskRun));
+
+  @override
+  _$_TaskRun get _value => super._value as _$_TaskRun;
+
+  @override
+  $Res call({
+    Object? taskConfig = freezed,
+    Object? taskRunId = freezed,
+    Object? taskState = freezed,
+  }) {
+    return _then(_$_TaskRun(
+      taskConfig: taskConfig == freezed
+          ? _value.taskConfig
+          : taskConfig // ignore: cast_nullable_to_non_nullable
+              as TaskConfig,
+      taskRunId: taskRunId == freezed
+          ? _value.taskRunId
+          : taskRunId // ignore: cast_nullable_to_non_nullable
+              as String,
+      taskState: taskState == freezed
+          ? _value.taskState
+          : taskState // ignore: cast_nullable_to_non_nullable
+              as TaskState,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_TaskRun implements _TaskRun {
+  const _$_TaskRun(
+      {required this.taskConfig,
+      required this.taskRunId,
+      required this.taskState});
+
+  @override
+  final TaskConfig taskConfig;
+  @override
+  final String taskRunId;
+  @override
+  final TaskState taskState;
+
+  @override
+  String toString() {
+    return 'TaskRun(taskConfig: $taskConfig, taskRunId: $taskRunId, taskState: $taskState)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_TaskRun &&
+            const DeepCollectionEquality()
+                .equals(other.taskConfig, taskConfig) &&
+            const DeepCollectionEquality().equals(other.taskRunId, taskRunId) &&
+            const DeepCollectionEquality().equals(other.taskState, taskState));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(taskConfig),
+      const DeepCollectionEquality().hash(taskRunId),
+      const DeepCollectionEquality().hash(taskState));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_TaskRunCopyWith<_$_TaskRun> get copyWith =>
+      __$$_TaskRunCopyWithImpl<_$_TaskRun>(this, _$identity);
+}
+
+abstract class _TaskRun implements TaskRun {
+  const factory _TaskRun(
+      {required final TaskConfig taskConfig,
+      required final String taskRunId,
+      required final TaskState taskState}) = _$_TaskRun;
+
+  @override
+  TaskConfig get taskConfig;
+  @override
+  String get taskRunId;
+  @override
+  TaskState get taskState;
+  @override
+  @JsonKey(ignore: true)
+  _$$_TaskRunCopyWith<_$_TaskRun> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
 TaskConfig _$TaskConfigFromJson(Map<String, dynamic> json) {
-  return TaskConfigImageUpload.fromJson(json);
+  switch (json['type']) {
+    case 'imageUpload':
+      return TaskConfigImageUpload.fromJson(json);
+    case 'createImage':
+      return TaskConfigCreateImage.fromJson(json);
+
+    default:
+      throw CheckedFromJsonException(
+          json, 'type', 'TaskConfig', 'Invalid union type "${json['type']}"!');
+  }
 }
 
 /// @nodoc
 mixin _$TaskConfig {
-  String get externalId => throw _privateConstructorUsedError;
+  String get groupId => throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult when<TResult extends Object?>({
-    required TResult Function(String externalId) imageUpload,
+    required TResult Function(String externalId, String groupId) imageUpload,
+    required TResult Function(String groupId) createImage,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult? whenOrNull<TResult extends Object?>({
-    TResult Function(String externalId)? imageUpload,
+    TResult Function(String externalId, String groupId)? imageUpload,
+    TResult Function(String groupId)? createImage,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult maybeWhen<TResult extends Object?>({
-    TResult Function(String externalId)? imageUpload,
+    TResult Function(String externalId, String groupId)? imageUpload,
+    TResult Function(String groupId)? createImage,
     required TResult orElse(),
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult map<TResult extends Object?>({
     required TResult Function(TaskConfigImageUpload value) imageUpload,
+    required TResult Function(TaskConfigCreateImage value) createImage,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult? mapOrNull<TResult extends Object?>({
     TResult Function(TaskConfigImageUpload value)? imageUpload,
+    TResult Function(TaskConfigCreateImage value)? createImage,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult maybeMap<TResult extends Object?>({
     TResult Function(TaskConfigImageUpload value)? imageUpload,
+    TResult Function(TaskConfigCreateImage value)? createImage,
     required TResult orElse(),
   }) =>
       throw _privateConstructorUsedError;
@@ -64,7 +242,7 @@ abstract class $TaskConfigCopyWith<$Res> {
   factory $TaskConfigCopyWith(
           TaskConfig value, $Res Function(TaskConfig) then) =
       _$TaskConfigCopyWithImpl<$Res>;
-  $Res call({String externalId});
+  $Res call({String groupId});
 }
 
 /// @nodoc
@@ -77,12 +255,12 @@ class _$TaskConfigCopyWithImpl<$Res> implements $TaskConfigCopyWith<$Res> {
 
   @override
   $Res call({
-    Object? externalId = freezed,
+    Object? groupId = freezed,
   }) {
     return _then(_value.copyWith(
-      externalId: externalId == freezed
-          ? _value.externalId
-          : externalId // ignore: cast_nullable_to_non_nullable
+      groupId: groupId == freezed
+          ? _value.groupId
+          : groupId // ignore: cast_nullable_to_non_nullable
               as String,
     ));
   }
@@ -95,7 +273,7 @@ abstract class _$$TaskConfigImageUploadCopyWith<$Res>
           $Res Function(_$TaskConfigImageUpload) then) =
       __$$TaskConfigImageUploadCopyWithImpl<$Res>;
   @override
-  $Res call({String externalId});
+  $Res call({String externalId, String groupId});
 }
 
 /// @nodoc
@@ -112,11 +290,16 @@ class __$$TaskConfigImageUploadCopyWithImpl<$Res>
   @override
   $Res call({
     Object? externalId = freezed,
+    Object? groupId = freezed,
   }) {
     return _then(_$TaskConfigImageUpload(
       externalId: externalId == freezed
           ? _value.externalId
           : externalId // ignore: cast_nullable_to_non_nullable
+              as String,
+      groupId: groupId == freezed
+          ? _value.groupId
+          : groupId // ignore: cast_nullable_to_non_nullable
               as String,
     ));
   }
@@ -125,17 +308,25 @@ class __$$TaskConfigImageUploadCopyWithImpl<$Res>
 /// @nodoc
 @JsonSerializable()
 class _$TaskConfigImageUpload extends TaskConfigImageUpload {
-  const _$TaskConfigImageUpload({required this.externalId}) : super._();
+  const _$TaskConfigImageUpload(
+      {required this.externalId, required this.groupId, final String? $type})
+      : $type = $type ?? 'imageUpload',
+        super._();
 
   factory _$TaskConfigImageUpload.fromJson(Map<String, dynamic> json) =>
       _$$TaskConfigImageUploadFromJson(json);
 
   @override
   final String externalId;
+  @override
+  final String groupId;
+
+  @JsonKey(name: 'type')
+  final String $type;
 
   @override
   String toString() {
-    return 'TaskConfig.imageUpload(externalId: $externalId)';
+    return 'TaskConfig.imageUpload(externalId: $externalId, groupId: $groupId)';
   }
 
   @override
@@ -144,13 +335,16 @@ class _$TaskConfigImageUpload extends TaskConfigImageUpload {
         (other.runtimeType == runtimeType &&
             other is _$TaskConfigImageUpload &&
             const DeepCollectionEquality()
-                .equals(other.externalId, externalId));
+                .equals(other.externalId, externalId) &&
+            const DeepCollectionEquality().equals(other.groupId, groupId));
   }
 
   @JsonKey(ignore: true)
   @override
-  int get hashCode =>
-      Object.hash(runtimeType, const DeepCollectionEquality().hash(externalId));
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(externalId),
+      const DeepCollectionEquality().hash(groupId));
 
   @JsonKey(ignore: true)
   @override
@@ -161,27 +355,30 @@ class _$TaskConfigImageUpload extends TaskConfigImageUpload {
   @override
   @optionalTypeArgs
   TResult when<TResult extends Object?>({
-    required TResult Function(String externalId) imageUpload,
+    required TResult Function(String externalId, String groupId) imageUpload,
+    required TResult Function(String groupId) createImage,
   }) {
-    return imageUpload(externalId);
+    return imageUpload(externalId, groupId);
   }
 
   @override
   @optionalTypeArgs
   TResult? whenOrNull<TResult extends Object?>({
-    TResult Function(String externalId)? imageUpload,
+    TResult Function(String externalId, String groupId)? imageUpload,
+    TResult Function(String groupId)? createImage,
   }) {
-    return imageUpload?.call(externalId);
+    return imageUpload?.call(externalId, groupId);
   }
 
   @override
   @optionalTypeArgs
   TResult maybeWhen<TResult extends Object?>({
-    TResult Function(String externalId)? imageUpload,
+    TResult Function(String externalId, String groupId)? imageUpload,
+    TResult Function(String groupId)? createImage,
     required TResult orElse(),
   }) {
     if (imageUpload != null) {
-      return imageUpload(externalId);
+      return imageUpload(externalId, groupId);
     }
     return orElse();
   }
@@ -190,6 +387,7 @@ class _$TaskConfigImageUpload extends TaskConfigImageUpload {
   @optionalTypeArgs
   TResult map<TResult extends Object?>({
     required TResult Function(TaskConfigImageUpload value) imageUpload,
+    required TResult Function(TaskConfigCreateImage value) createImage,
   }) {
     return imageUpload(this);
   }
@@ -198,6 +396,7 @@ class _$TaskConfigImageUpload extends TaskConfigImageUpload {
   @optionalTypeArgs
   TResult? mapOrNull<TResult extends Object?>({
     TResult Function(TaskConfigImageUpload value)? imageUpload,
+    TResult Function(TaskConfigCreateImage value)? createImage,
   }) {
     return imageUpload?.call(this);
   }
@@ -206,6 +405,7 @@ class _$TaskConfigImageUpload extends TaskConfigImageUpload {
   @optionalTypeArgs
   TResult maybeMap<TResult extends Object?>({
     TResult Function(TaskConfigImageUpload value)? imageUpload,
+    TResult Function(TaskConfigCreateImage value)? createImage,
     required TResult orElse(),
   }) {
     if (imageUpload != null) {
@@ -223,17 +423,179 @@ class _$TaskConfigImageUpload extends TaskConfigImageUpload {
 }
 
 abstract class TaskConfigImageUpload extends TaskConfig {
-  const factory TaskConfigImageUpload({required final String externalId}) =
-      _$TaskConfigImageUpload;
+  const factory TaskConfigImageUpload(
+      {required final String externalId,
+      required final String groupId}) = _$TaskConfigImageUpload;
   const TaskConfigImageUpload._() : super._();
 
   factory TaskConfigImageUpload.fromJson(Map<String, dynamic> json) =
       _$TaskConfigImageUpload.fromJson;
 
-  @override
   String get externalId;
+  @override
+  String get groupId;
   @override
   @JsonKey(ignore: true)
   _$$TaskConfigImageUploadCopyWith<_$TaskConfigImageUpload> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class _$$TaskConfigCreateImageCopyWith<$Res>
+    implements $TaskConfigCopyWith<$Res> {
+  factory _$$TaskConfigCreateImageCopyWith(_$TaskConfigCreateImage value,
+          $Res Function(_$TaskConfigCreateImage) then) =
+      __$$TaskConfigCreateImageCopyWithImpl<$Res>;
+  @override
+  $Res call({String groupId});
+}
+
+/// @nodoc
+class __$$TaskConfigCreateImageCopyWithImpl<$Res>
+    extends _$TaskConfigCopyWithImpl<$Res>
+    implements _$$TaskConfigCreateImageCopyWith<$Res> {
+  __$$TaskConfigCreateImageCopyWithImpl(_$TaskConfigCreateImage _value,
+      $Res Function(_$TaskConfigCreateImage) _then)
+      : super(_value, (v) => _then(v as _$TaskConfigCreateImage));
+
+  @override
+  _$TaskConfigCreateImage get _value => super._value as _$TaskConfigCreateImage;
+
+  @override
+  $Res call({
+    Object? groupId = freezed,
+  }) {
+    return _then(_$TaskConfigCreateImage(
+      groupId: groupId == freezed
+          ? _value.groupId
+          : groupId // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$TaskConfigCreateImage extends TaskConfigCreateImage {
+  const _$TaskConfigCreateImage({required this.groupId, final String? $type})
+      : $type = $type ?? 'createImage',
+        super._();
+
+  factory _$TaskConfigCreateImage.fromJson(Map<String, dynamic> json) =>
+      _$$TaskConfigCreateImageFromJson(json);
+
+  @override
+  final String groupId;
+
+  @JsonKey(name: 'type')
+  final String $type;
+
+  @override
+  String toString() {
+    return 'TaskConfig.createImage(groupId: $groupId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TaskConfigCreateImage &&
+            const DeepCollectionEquality().equals(other.groupId, groupId));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(groupId));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$TaskConfigCreateImageCopyWith<_$TaskConfigCreateImage> get copyWith =>
+      __$$TaskConfigCreateImageCopyWithImpl<_$TaskConfigCreateImage>(
+          this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(String externalId, String groupId) imageUpload,
+    required TResult Function(String groupId) createImage,
+  }) {
+    return createImage(groupId);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function(String externalId, String groupId)? imageUpload,
+    TResult Function(String groupId)? createImage,
+  }) {
+    return createImage?.call(groupId);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(String externalId, String groupId)? imageUpload,
+    TResult Function(String groupId)? createImage,
+    required TResult orElse(),
+  }) {
+    if (createImage != null) {
+      return createImage(groupId);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(TaskConfigImageUpload value) imageUpload,
+    required TResult Function(TaskConfigCreateImage value) createImage,
+  }) {
+    return createImage(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(TaskConfigImageUpload value)? imageUpload,
+    TResult Function(TaskConfigCreateImage value)? createImage,
+  }) {
+    return createImage?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(TaskConfigImageUpload value)? imageUpload,
+    TResult Function(TaskConfigCreateImage value)? createImage,
+    required TResult orElse(),
+  }) {
+    if (createImage != null) {
+      return createImage(this);
+    }
+    return orElse();
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$TaskConfigCreateImageToJson(
+      this,
+    );
+  }
+}
+
+abstract class TaskConfigCreateImage extends TaskConfig {
+  const factory TaskConfigCreateImage({required final String groupId}) =
+      _$TaskConfigCreateImage;
+  const TaskConfigCreateImage._() : super._();
+
+  factory TaskConfigCreateImage.fromJson(Map<String, dynamic> json) =
+      _$TaskConfigCreateImage.fromJson;
+
+  @override
+  String get groupId;
+  @override
+  @JsonKey(ignore: true)
+  _$$TaskConfigCreateImageCopyWith<_$TaskConfigCreateImage> get copyWith =>
       throw _privateConstructorUsedError;
 }
--- app/lib/repositories/task_repository.g.dart
+++ app/lib/repositories/task_repository.g.dart
@@ -10,10 +10,28 @@ _$TaskConfigImageUpload _$$TaskConfigImageUploadFromJson(
         Map<String, dynamic> json) =>
     _$TaskConfigImageUpload(
       externalId: json['externalId'] as String,
+      groupId: json['groupId'] as String,
+      $type: json['type'] as String?,
     );
 
 Map<String, dynamic> _$$TaskConfigImageUploadToJson(
         _$TaskConfigImageUpload instance) =>
     <String, dynamic>{
       'externalId': instance.externalId,
+      'groupId': instance.groupId,
+      'type': instance.$type,
+    };
+
+_$TaskConfigCreateImage _$$TaskConfigCreateImageFromJson(
+        Map<String, dynamic> json) =>
+    _$TaskConfigCreateImage(
+      groupId: json['groupId'] as String,
+      $type: json['type'] as String?,
+    );
+
+Map<String, dynamic> _$$TaskConfigCreateImageToJson(
+        _$TaskConfigCreateImage instance) =>
+    <String, dynamic>{
+      'groupId': instance.groupId,
+      'type': instance.$type,
     };

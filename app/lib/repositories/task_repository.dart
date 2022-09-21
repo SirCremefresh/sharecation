81c2cf0b6f97c21b903f0206e5301e6dcc4e9c82
donatowolfisberg <donato@wolfisberg.dev>
2022-09-21 20:02:50 +0200

    run tasks asynchron

--- app/lib/repositories/task_repository.dart
+++ app/lib/repositories/task_repository.dart
@@ -8,32 +8,46 @@ part 'task_repository.freezed.dart';
 
 part 'task_repository.g.dart';
 
-//
-// flutter -> tasks, bloc
-// bloc -> api
-// api
-// tasks -> api, bloc
-//
-
 class TaskRepository {
   final MainBloc mainBloc;
   final TaskBloc taskBloc;
 
-  TaskRepository({required this.mainBloc, required this.taskBloc});
+  TaskRepository({required this.mainBloc, required this.taskBloc}) {
+    startScheduler();
+  }
+
+  void startScheduler() {
+    Future.microtask(() async {
+      while (true) {
+        await Future.delayed(const Duration(seconds: 5));
+        await runTasks();
+      }
+    });
+  }
+
+  Future<void> runTasks() async {
+    for (var element in taskBloc.state.taskRuns) {
+      if (element.taskState != TaskState.created) {
+        continue;
+      }
+      final task = getTaskForTaskConfig(element.taskConfig);
+      await task.start(mainBloc, this);
+      taskBloc.add(
+        TaskEvent.upsertTaskConfig(
+          taskRun: element.copyWith(taskState: TaskState.succeeded),
+        ),
+      );
+    }
+  }
 
   Future<void> addTask(TaskConfig taskConfig) async {
     var taskRun = TaskRun(
-        taskConfig: taskConfig,
-        taskRunId: const Uuid().v4(),
-        taskState: TaskState.created);
-    taskBloc.add(TaskEvent.upsertTaskConfig(taskRun: taskRun));
-    final task = getTaskForTaskConfig(taskConfig);
-    await task.start(mainBloc, this);
-    taskBloc.add(
-      TaskEvent.upsertTaskConfig(
-        taskRun: taskRun.copyWith(taskState: TaskState.finished),
-      ),
+      taskConfig: taskConfig,
+      taskRunId: const Uuid().v4(),
+      createdAt: DateTime.now(),
+      taskState: TaskState.created,
     );
+    taskBloc.add(TaskEvent.upsertTaskConfig(taskRun: taskRun));
   }
 
   Task getTaskForTaskConfig(TaskConfig taskConfig) => taskConfig.map(
@@ -47,14 +61,16 @@ abstract class Task {
   void stop();
 }
 
-enum TaskState { created, finished }
+enum TaskState { created, running, succeeded, failed }
 
 @freezed
 class TaskRun with _$TaskRun {
-  const factory TaskRun(
-      {required TaskConfig taskConfig,
-      required String taskRunId,
-      required TaskState taskState}) = _TaskRun;
+  const factory TaskRun({
+    required TaskConfig taskConfig,
+    required String taskRunId,
+    required DateTime createdAt,
+    required TaskState taskState,
+  }) = _TaskRun;
 }
 
 @Freezed(unionKey: 'type')
@@ -74,9 +90,9 @@ class TaskConfig with _$TaskConfig {
       _$TaskConfigFromJson(json);
 
   String getName() => map(
-    imageUpload: (_) => "image_upload",
-    createImage: (_) => "create_image",
-  );
+        imageUpload: (_) => "image_upload",
+        createImage: (_) => "create_image",
+      );
 }
 
 class PictureUploadTask extends Task {
